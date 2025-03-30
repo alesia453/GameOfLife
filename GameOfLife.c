6cbf761410c9900040db1b450051e38354ac3a7f
alesiagilma@gmail.com <alesiaa@ByteMe.>
2025-03-30 23:06:47 +0300

    implementare reguli: Fix pentru optimizare si style

--- GameOfLife.c
+++ GameOfLife.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #include <string.h>
 FILE *finput, *foutput;
-void reead(char **grid, int *n, int *m, int *k)
+void reead(char ***grid, int *n, int *m, int *k)
 {
 
     int i,j;
@@ -14,19 +14,20 @@ void reead(char **grid, int *n, int *m, int *k)
     bordern=(*n)+2;
     borderm=(*m)+2;
 
-    grid=(char**)malloc(bordern * sizeof(char*));
+    *grid=(char**)malloc(bordern * sizeof(char*));
     for(i=0;i<bordern;i++)
     {   
-        (grid)[i]=(char*)malloc((borderm+1) * sizeof(char));
+        (*grid)[i]=(char*)malloc((borderm+1) * sizeof(char));
     }
     //initializez borderul
     for(i=0;i<bordern;i++)
         for(j=0;j<borderm;j++)
         {
-            (grid)[0][j]='?';
-            (grid)[bordern-1][j]='?';
-            (grid)[i][0]='?';
-            (grid)[i][borderm-1]='?';
+            (*grid)[i][j]='+';
+            (*grid)[0][j]='?';
+            (*grid)[bordern-1][j]='?';
+            (*grid)[i][0]='?';
+            (*grid)[i][borderm-1]='?';
         }
     
     //citesc restul valorilor in interiorul borderului
@@ -34,38 +35,35 @@ void reead(char **grid, int *n, int *m, int *k)
     for(i=1;i<=*n;i++)
         for(j=1;j<=*m;j++)
     {   
-        fscanf(finput, " %c", &grid[i][j]);
+        fscanf(finput, " %c", &((*grid)[i][j]));
     }
     
 }
-void writee(char **grid, int *n, int *m)
+void writee(char **grid, int n, int m)
 {
     int i,j;
-    for(i=1;i<=*n;i++)
+    for(i=1;i<=n;i++)
     {
-        for(j=1;j<=*m;j++)
+        for(j=1;j<=m;j++){
+            //printf("%c",grid[i][j]);
             fprintf(foutput, "%c", grid[i][j]);
+        }
+        //printf("\n");
         fprintf(foutput,"\n");
     }
 }
 
-void freespace( char **grid, int *n)
+void freespace( char **grid, int n)
 {
     int i,bordern;
-    bordern=(*n)+2;
+    bordern=(n)+2;
     for(i=0;i<bordern;i++)
         free(grid[i]);
     free(grid);
 }
-int alive_neighbours(char **grid, int n, int m,int i, int j)
+int alive_neighbours(char **grid, int i, int j)
 {
     int how_many=0;
-    /*for(i=1;i<n;i++)
-        for(j=1;j<m;j++)
-        {
-            if((grid)[i-1][j]=='X' || (grid)[i][j+1]=='X' || (grid)[i+1][j]=='X' || (grid)[i][j-1]=='X') 
-                how_many++;
-        }*/
     if(grid[i-1][j]=='X') how_many++;
     if(grid[i+1][j]=='X') how_many++;
     if(grid[i-1][j-1]=='X') how_many++;
@@ -74,48 +72,46 @@ int alive_neighbours(char **grid, int n, int m,int i, int j)
     if(grid[i+1][j+1]=='X') how_many++;
     if(grid[i][j+1]=='X') how_many++;
     if(grid[i][j-1]=='X') how_many++;
-   return how_many;
+    return how_many;
 }
-void new_gen(char **grid, int *n, int *m, char ***newgen)
+void new_gen(char **grid, int n, int m, char ***newgen)
 {
     int i, j, alive;
     //aloc memorie pt noua generatie cu tot cu border
     int bordern, borderm;
-    bordern=(*n)+2;
-    borderm=(*m)+2;
+    bordern=(n)+2;
+    borderm=(m)+2;
 
     *newgen=(char**)malloc(bordern * sizeof(char*));
     for(i=0;i<bordern;i++)
     {   
-        (*newgen)[i]=(char*)malloc((borderm) * sizeof(char));
+        (*newgen)[i]=(char*)malloc((borderm + 1) * sizeof(char));
     }
     //initializez borderul
     for(i=0;i<bordern;i++)
         for(j=0;j<borderm;j++)
         {
+            (*newgen)[i][j]='+';
             (*newgen)[0][j]='?';
             (*newgen)[bordern-1][j]='?';
             (*newgen)[i][0]='?';
             (*newgen)[i][borderm-1]='?';
         }
     //parcurg veche generatie si salvez modificarile in noua generatie
-    for(i=1;i<=*n;i++)
-        for(j=1;j<=*m;j++)
+    for(i=1;i<=n;i++)
+        for(j=1;j<=m;j++)
         {
-            alive=alive_neighbours(grid,*n,*m,i,j);
+            alive=alive_neighbours(grid,i,j);
             if((grid)[i][j]=='X')
             {
-                if(alive<2) (*newgen)[i][j]='+';
+                if(alive<2 || alive>3) (*newgen)[i][j]='+';
                 else
-                    if(alive==2 || alive==3) (*newgen)[i][j]='X';
-                    else
-                        if(alive>3)(*newgen)[i][j]='+';
+                    if(alive==2 || alive==3) 
+                        (*newgen)[i][j]='X';
+            }else
+                if(alive==3)(*newgen)[i][j]='X';
+                    else (*newgen)[i][j]=(grid)[i][j];
             }
-            else 
-            if(alive==3) (*newgen)[i][j]='X';
-            else (*newgen)[i][j]=(grid)[i][j];
-
-        }
 
 }
 void swap_gen(char ***grid, char ***newgen)
@@ -127,15 +123,17 @@ void swap_gen(char ***grid, char ***newgen)
     *newgen=aux;
 }
 
-int main(int argc, char* argv[])
+int main(int argc, const char* argv[])
 {
-    int i,j,*n,*m,*k,generation;
-    char **grid,**newgen;
-   
-    //for(i=1;i<argc;i+=2)
-    //{
-        finput=fopen(argv[1],"r");
-        foutput=fopen(argv[2],"w");
+    int i,*n,*m,*k,generation;
+    char **grid=NULL,**newgen=NULL;
+    n=(int*)malloc(sizeof(int));
+    m=(int*)malloc(sizeof(int));
+    k=(int*)malloc(sizeof(int));
+    for(i=1;i<argc;i+=2)
+    {
+        finput=fopen(argv[i],"r");
+        foutput=fopen(argv[i+1],"w");
         if(finput==NULL)
     {
         puts("Fisierul input nu poate fi deschis!\n");
@@ -147,22 +145,25 @@ int main(int argc, char* argv[])
         exit(1);
     }
 
-    reead(grid,n,m,k);
-    //writee(grid,n,m);
+    reead(&grid,n,m,k);
+    
         for(generation=0;generation<*k;generation++)
             {
-                new_gen(grid,n,m,&newgen);
-                writee(newgen,n,m);
+                new_gen(grid,*n,*m,&newgen);
+                writee(newgen,*n,*m);
                 swap_gen(&grid,&newgen);
+                //freespace(newgen,*n);
             }
 
-    freespace(grid,n);
-    freespace(newgen,n);
+    writee(grid,*n,*m);
+    freespace(grid,*n);
+    
     
 
     fclose(finput); 
     fclose(foutput);
 
-    //}
+    }
+    free(n); free(m); free(k);
     return 0;
 }
